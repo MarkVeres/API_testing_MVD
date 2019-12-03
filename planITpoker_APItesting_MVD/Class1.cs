89ceaee508446a3be31f7cbae60e5ea499707319
MarkVeres <56633682+MarkVeres@users.noreply.github.com>
2019-12-03 15:00:16 +0200

    first commit

---
+++ planITpoker_APItesting_MVD/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace planITpoker_APItesting_MVD
+{
+    public class Class1
+    {
+        public void Beep()
+        {
+            Console.Beep();
+        }
+        public void SecondBeep()
+        {
+            Console.Beep();
+        }
+    }
+}
