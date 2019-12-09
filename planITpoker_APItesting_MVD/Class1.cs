ab0f0afe8ebaf5cde30deac667f108ea52ae1c57
MarkVeres <56633682+MarkVeres@users.noreply.github.com>
2019-12-09 16:39:09 +0200

    first try API testing

--- planITpoker_APItesting_MVD/Class1.cs
+++ planITpoker_APItesting_MVD/Class1.cs
@@ -1,16 +1,58 @@
 ﻿using System;
+using Xunit;
+using Refit;
+using System.Threading.Tasks;
 
 namespace planITpoker_APItesting_MVD
 {
-    public class Class1
+    public class User
     {
-        public void Beep()
+        public string Name { get; set; }
+    }
+    public interface IGitHubApi
+    {
+        [Get("/users/{user}")]
+        Task<User> GetUser(string user);
+
+        [Get("")]
+        Task<string> GetGoogle();
+
+        [Get("/quickplay/")]
+        Task<string> GetQuick();
+
+        [Get("/board/")]
+        Task<string> GetBoard();
+    }
+    public class UnitTest1
+    {
+        [Fact]
+        public async Task Test1()
         {
-            Console.Beep();
+            var gitHubApi = RestService.For<IGitHubApi>("https://api.github.com");
+            var octocat = await gitHubApi.GetUser("octocat");
+            Assert.Equal("octocat", octocat.Name);
         }
-        public void SecondBeep()
+        [Fact]
+        public async Task planIT()
         {
-            Console.Beep();
+            var planITAPI = RestService.For<IGitHubApi>("https://www.planitpoker.com/");
+            var quick = await planITAPI.GetQuick();
+            Assert.NotNull(quick);
+        }
+        [Fact]
+        public async Task TestGoogle()
+        {
+            var googleAPI = RestService.For<IGitHubApi>("https://www.google.com/");
+            var google = await googleAPI.GetGoogle();
+            Assert.NotNull(google);
+        }
+        [Fact]
+        public async Task TestBoard()
+        {
+            var googleAPI = RestService.For<IGitHubApi>("https://www.planitpoker.com/");
+            var google = await googleAPI.GetBoard();
+            Assert.NotNull(google);
         }
     }
+
 }
