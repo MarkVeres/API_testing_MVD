619a7ce96709ebd212b14481c9e481eeaad55999
MarkVeres <56633682+MarkVeres@users.noreply.github.com>
2019-12-10 14:23:30 +0200

    test start and model

--- planITpoker_APItesting_MVD/Class1.cs
+++ planITpoker_APItesting_MVD/Class1.cs
@@ -5,53 +5,18 @@ using System.Threading.Tasks;
 
 namespace planITpoker_APItesting_MVD
 {
-    public class User
+    public interface IplanITpokerAPI
     {
-        public string Name { get; set; }
-    }
-    public interface IGitHubApi
-    {
-        [Get("/users/{user}")]
-        Task<User> GetUser(string user);
-
-        [Get("")]
-        Task<string> GetGoogle();
-
-        [Get("/quickplay/")]
-        Task<string> GetQuick();
-
-        [Get("/board/")]
-        Task<string> GetBoard();
+        [Post("/quickplay/")]
+        Task<string> PostUserName(string name);
     }
     public class UnitTest1
-    {
-        [Fact]
-        public async Task Test1()
-        {
-            var gitHubApi = RestService.For<IGitHubApi>("https://api.github.com");
-            var octocat = await gitHubApi.GetUser("octocat");
-            Assert.Equal("octocat", octocat.Name);
-        }
+    {        
         [Fact]
         public async Task planIT()
         {
-            var planITAPI = RestService.For<IGitHubApi>("https://www.planitpoker.com/");
-            var quick = await planITAPI.GetQuick();
-            Assert.NotNull(quick);
-        }
-        [Fact]
-        public async Task TestGoogle()
-        {
-            var googleAPI = RestService.For<IGitHubApi>("https://www.google.com/");
-            var google = await googleAPI.GetGoogle();
-            Assert.NotNull(google);
-        }
-        [Fact]
-        public async Task TestBoard()
-        {
-            var googleAPI = RestService.For<IGitHubApi>("https://www.planitpoker.com/");
-            var google = await googleAPI.GetBoard();
-            Assert.NotNull(google);
+            var planITAPI = RestService.For<IplanITpokerAPI>("https://www.planitpoker.com/");
+            var quick = await planITAPI.PostUserName("John");
         }
     }
 
---
+++ planITpoker_APItesting_MVD/workingModel.cs
@@ -0,0 +1,30 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace XUnitTestProject2
+{
+    public class User
+    {
+        public string Name { get; set; }
+    }
+    public interface IGitHubApi
+    {
+        [Headers("User-Agent: Awesome Octocat App")]
+        [Get("/users/{user}")]
+        Task<User> GetUser(string user);
+    }
+    public class UnitTest1
+    {
+        [Fact]
+        public async Task Test1()
+        {
+            var gitHubApi = RestService.For<IGitHubApi>("https://api.github.com");
+            var octocat = await gitHubApi.GetUser("octocat");
+            Assert.Equal("The Octocat", octocat.Name);
+        }
+    }
+}
