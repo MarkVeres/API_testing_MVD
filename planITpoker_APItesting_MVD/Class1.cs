40d7dae1e0472aa7e555c3d324b1770cd9eedaed
MarkVeres <56633682+MarkVeres@users.noreply.github.com>
2019-12-12 11:28:30 +0200

    second try

--- planITpoker_APItesting_MVD/Class1.cs
+++ planITpoker_APItesting_MVD/Class1.cs
@@ -5,18 +5,23 @@ using System.Threading.Tasks;
 
 namespace planITpoker_APItesting_MVD
 {
+    public class User
+    {
+        public string Name { get; set; }
+    }
     public interface IplanITpokerAPI
     {
-        [Post("/quickplay/")]
-        Task<string> PostUserName(string name);
+        [Post("/api/authentication/{user}")]
+        Task<User> PostUserName(string user);
     }
     public class UnitTest1
     {        
         [Fact]
-        public async Task planIT()
+        public async Task QuickPlayLogin()
         {
             var planITAPI = RestService.For<IplanITpokerAPI>("https://www.planitpoker.com/");
             var quick = await planITAPI.PostUserName("John");
+            Assert.Equal("name=John", quick.Name);
         }
     }
 
