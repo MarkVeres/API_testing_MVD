9a86dec2fbc533758ac0c6cbc2af76d4a2e70482
MarkVeres <56633682+MarkVeres@users.noreply.github.com>
2019-12-17 12:34:28 +0200

    classes implemented

---
+++ restSharp_Try/User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace restSharp_Try
+{
+    public class User
+    {
+        public string name { get; set; }
+    }
+}
