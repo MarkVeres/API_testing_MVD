04530eb6ffc04a6e4fb9e27eef30c88502ae47bd
MarkVeres <56633682+MarkVeres@users.noreply.github.com>
2019-12-18 09:51:57 +0200

    status check

--- restSharp_Try/API_test class.cs
+++ restSharp_Try/API_test class.cs
@@ -13,14 +13,27 @@ namespace restSharp_Try
         {
             RestSharpHelper restApi = new RestSharpHelper();
             var client = restApi.SetUrl("/api/authentication/anonymous");
-            var request = restApi.CreatePostRequest("name=Jack");
+            var request = restApi.CreatePostRequest();
             IRestResponse response = client.Execute(request);
 
-            int resLength = response.Content.Length;
+            //var client = new RestClient("https://www.planitpoker.com/api/authentication/anonymous");
+            //var request = new RestRequest(Method.POST);
+            //request.AddHeader("cache-control", "no-cache");
+            //request.AddHeader("Connection", "keep-alive");
+            //request.AddHeader("Cookie", "__cfduid=d5b74a13e6923e71a4997cdcb3c26b3291575446976; .AspNet.TwoFactorRememberBrowser=9KcmnpG0WDReU5M-3rP-SJZo2kROvz2UB9yitPpW8kHDGzC6axnFyOKLitencNZhby1nWtXeBmaY4wbfNPP09fANQxQrk9spS1QXhM-X_Jn0zJNe6zywKpc3Q-gZUzcm8tX1UtN9JvAlywAMguh0rgmoXkJZYuzIEqFNNh38xKouYUnbQzs72Fa22F78GLlV-qw_3YAG0LPQquMwcGDSH3F4RH4ufYMME6cCDWenIoioS485_KKuXRHy6nAqdnoFIy-a6Zj4opuu2q5j9SyktxtKObA_jOj7Z9Y4m0c0OT5tAsgrqphNco-SP2fRN_o6; .AspNet.ApplicationCookie=Kl-ZvYKldYbkUJZxItXjwhoV9QHjpNThmV3HFjqacNlSfP6j3bzh__W8BXzqWcXrKgfZLXeMrqn9IkZTsFxknZQLeCUBFqxd2e6DDMAP0klImaNJXwgTOdTlIF6-WINWfgMs-M81RuoLpwEtv1vdXxelRUHrHGX-gavKSR806L3QclxzTU4YW1aPV6Sq7_C6O4HZu_kwUhQozKW5w_v0HILOVbhjO50LmD_RWaQkcz8XEeFdW6xm0DJ4rYf9plN1wvTlVh1Sa8-L-IZndCWqUg9Xjy49jP8ifSJ9duhv_GBQbXxOa0sb3qkbX1OLe1IdDyVpbPJSuIxgJsVUTciO5uNW0RWvjMT9sBsSMg_6xyRyaOhvcgKDXK3UIIRGc8Wq5uVMH2AS9HY5DLrkwQBQZs7VdNOgkOKGdADu5exOzdY");
+            //request.AddHeader("Content-Length", "9");
+            //request.AddHeader("Accept-Encoding", "gzip, deflate");
+            //request.AddHeader("Host", "www.planitpoker.com");
+            //request.AddHeader("Postman-Token", "157aa182-971a-42eb-ac4f-22f3cfd1428c,2fdb85b6-5ecb-46ff-8a4c-306f30b7281f");
+            //request.AddHeader("Cache-Control", "no-cache");
+            //request.AddHeader("Accept", "*/*");
+            //request.AddHeader("User-Agent", "PostmanRuntime/7.20.1");
+            //request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+            //request.AddParameter("undefined", "name=Jack", ParameterType.RequestBody);
+            //IRestResponse response = client.Execute(request);
 
-            //Length should be 0, although Postman returns length = 2
-            Assert.Equal(0, resLength);
-            // How to assert name from the request ???
+            string status = response.StatusCode.ToString();
+            Assert.Equal("OK", status);
         }
     }
 }
--- restSharp_Try/RestSharpHelper.cs
+++ restSharp_Try/RestSharpHelper.cs
@@ -20,18 +20,21 @@ namespace restSharp_Try
             return client;
         }
 
-        public RestRequest CreatePostRequest(string input)
+        public RestRequest CreatePostRequest()
         {
             request = new RestRequest(Method.POST);
             request.AddHeader("cache-control", "no-cache");
             request.AddHeader("Connection", "keep-alive");
-            request.AddHeader("Cookie", "__cfduid=d5b74a13e6923e71a4997cdcb3c26b3291575446976; .AspNet.TwoFactorRememberBrowser=6HshiEfKlXIA7ZEDkGq-niGp3Eqpc_qKFYMmShx_x21fGs_ieH8rhYahkYm_EI9mXKSpeS_lfydmiEaT1Wy14YZR9kkJWsnNMv8G1S3fRKsPMixPyXktWhtMsP5mCjXnMG6wy8E8kocn0Hat2nTpJxXzEEoivLOp4tCdu2EK137__8eUXvh29nX7N6mWD1mW4f-W4nLJKs745qlqBVeGt0IANurhf8ezQEqkUoXqXw5l_TfG00OoZpx3HADqs82Xf_ZPPAozX5acGEQgR_Lxp1eFjB01vOuYMhGMrI1IrErnSDPIU6al58GPMDZpJ1w6; .AspNet.ApplicationCookie=-gZtzrrupPJGK_7AGv2soltfbuO5I20vSZBj2suEwdWX-bU1xo7v8o2ZYhHDWu78pG1z1J4lyc5NQnSiE8nUirkkMnbJeDTLrNbBUXNp8vphC7BBC6gJXbTBT9vKPDSUdzn-JLfTrlkyuD3tV83Y46Squ2PXMe5BcPAHn80qLFFtq2J9MO38mw38FeAPK3pQ0uPpw5X2y0lsinwGgsNT8MgOZbIjn6Vb8mSdvobtzB4WhZXna27_0Y1OevKvd_M6eD9549ViDa85fdCTAZCSDS2Dhli8UblzF1REo5SifiCcdKoymaOcqqcAhUeYeBevWqD110Cv275kj9Uaj8vaNbd-HyCr_NvN5yq8uoPlXqAHiUplA-v-RXgPaZBihi4XhxqplflrnbyMvAqbrL8CC6O-zgSzfXLvFwCfFD1y-0g");
+            request.AddHeader("Cookie", "__cfduid=d5b74a13e6923e71a4997cdcb3c26b3291575446976; .AspNet.TwoFactorRememberBrowser=9KcmnpG0WDReU5M-3rP-SJZo2kROvz2UB9yitPpW8kHDGzC6axnFyOKLitencNZhby1nWtXeBmaY4wbfNPP09fANQxQrk9spS1QXhM-X_Jn0zJNe6zywKpc3Q-gZUzcm8tX1UtN9JvAlywAMguh0rgmoXkJZYuzIEqFNNh38xKouYUnbQzs72Fa22F78GLlV-qw_3YAG0LPQquMwcGDSH3F4RH4ufYMME6cCDWenIoioS485_KKuXRHy6nAqdnoFIy-a6Zj4opuu2q5j9SyktxtKObA_jOj7Z9Y4m0c0OT5tAsgrqphNco-SP2fRN_o6; .AspNet.ApplicationCookie=Kl-ZvYKldYbkUJZxItXjwhoV9QHjpNThmV3HFjqacNlSfP6j3bzh__W8BXzqWcXrKgfZLXeMrqn9IkZTsFxknZQLeCUBFqxd2e6DDMAP0klImaNJXwgTOdTlIF6-WINWfgMs-M81RuoLpwEtv1vdXxelRUHrHGX-gavKSR806L3QclxzTU4YW1aPV6Sq7_C6O4HZu_kwUhQozKW5w_v0HILOVbhjO50LmD_RWaQkcz8XEeFdW6xm0DJ4rYf9plN1wvTlVh1Sa8-L-IZndCWqUg9Xjy49jP8ifSJ9duhv_GBQbXxOa0sb3qkbX1OLe1IdDyVpbPJSuIxgJsVUTciO5uNW0RWvjMT9sBsSMg_6xyRyaOhvcgKDXK3UIIRGc8Wq5uVMH2AS9HY5DLrkwQBQZs7VdNOgkOKGdADu5exOzdY");
+            request.AddHeader("Content-Length", "9");
             request.AddHeader("Accept-Encoding", "gzip, deflate");
+            request.AddHeader("Host", "www.planitpoker.com");
+            request.AddHeader("Postman-Token", "157aa182-971a-42eb-ac4f-22f3cfd1428c,2fdb85b6-5ecb-46ff-8a4c-306f30b7281f");
             request.AddHeader("Cache-Control", "no-cache");
             request.AddHeader("Accept", "*/*");
             request.AddHeader("User-Agent", "PostmanRuntime/7.20.1");
             request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
-            request.AddParameter("undefined", input, ParameterType.RequestBody);
+            request.AddParameter("undefined", "name=Jack", ParameterType.RequestBody);
             return request;
         }
 
